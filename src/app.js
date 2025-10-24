e793d28ee4418ba155722aee9df86b1871aecdd8
Rohit Singh <rajputrohit2k@gmail.com>
2025-10-24 11:30:32 +0530

    playing with routes

--- src/app.js
+++ src/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("Hello from the  namste node server");
+app.get("/user/:userId", (req, res) => {
+  console.log(req.params);
+
+  res.send({ first_name: "Rohit", last_name: "Singh" });
 });
 
 app.listen(3000, () => {
