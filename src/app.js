49ddc28f35f4f0d83286dcd1c59fce391de29540
Rohit Singh <rajputrohit2k@gmail.com>
2025-10-27 11:42:08 +0530

    route handler

--- src/app.js
+++ src/app.js
@@ -2,11 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userId", (req, res) => {
-  console.log(req.params);
-
-  res.send({ first_name: "Rohit", last_name: "Singh" });
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    // route handler
+    console.log("route handler 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("route handler 2");
+    next();
+  },
+  (req, res, next) => {
+    console.log("route handler 3");
+    next();
+  },
+  (req, res) => {
+    res.send("route handler 4");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is succesfully lsiting on port have 3000...");
