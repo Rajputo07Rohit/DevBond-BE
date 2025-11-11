9b9fd3a0f1b606e38c569327fc0d857f501de8c8
Rohit Singh <rajputrohit2k@gmail.com>
2025-11-11 18:08:12 +0530

    API level data santization

--- src/app.js
+++ src/app.js
@@ -13,6 +13,9 @@ app.post("/signup", async (req, res) => {
   const user = new User(req.body);
 
   try {
+    if (user?.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
+    }
     await user.save();
     res.send("User Added successfully!");
   } catch (err) {
@@ -58,19 +61,35 @@ app.delete("/user", async (req, res) => {
   }
 });
 
-app.patch("/user", async (req, res) => {
-  console.log(req.body);
-  const userID = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  // console.log(req.body);
+  const userID = req.params?.userId;
   const data = req.body;
+
   try {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+
+    if (!isUpdateAllowed) {
+      throw new Error("Update is not allowed");
+    }
+
+    if (data?.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
+    }
+
     const user = await User.findByIdAndUpdate({ _id: userID }, data, {
       runValidators: true,
     });
+
     // const user = await User.findByIdAndDelete(userId);
 
     res.send("Data is Updated succesfully!");
   } catch (error) {
-    res.status(400).send("Something went wrong ");
+    res.status(400).send("Something went wrong " + error);
   }
 });
 
