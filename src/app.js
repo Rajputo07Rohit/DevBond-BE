4fb736c470509300fd3bf174eaffe34fc549f168
Rohit Singh <rajputrohit2k@gmail.com>
2025-11-11 17:40:27 +0530

    schema validation

--- src/app.js
+++ src/app.js
@@ -10,8 +10,6 @@ app.use(express.json());
 
 // Signup route
 app.post("/signup", async (req, res) => {
-  console.log(req.body);
-
   const user = new User(req.body);
 
   try {
@@ -20,6 +18,59 @@ app.post("/signup", async (req, res) => {
   } catch (err) {
     console.error(err);
     res.status(400).send("Error saving the user");
+  }
+});
+
+app.get("/user", async (req, res) => {
+  console.log(req.body);
+  const userEmail = req.body.emailId;
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(400).send("User not found");
+    } else {
+      res.send(users);
+    }
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (error) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  const userId = req.body.userId;
+
+  try {
+    const user = await User.findByIdAndDelete({ _id: userId });
+    // const user = await User.findByIdAndDelete(userId);
+
+    res.send("user is deleetd succesfully!");
+  } catch (error) {
+    res.status(400).send("Something went wrong ");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  console.log(req.body);
+  const userID = req.body.userId;
+  const data = req.body;
+  try {
+    const user = await User.findByIdAndUpdate({ _id: userID }, data, {
+      runValidators: true,
+    });
+    // const user = await User.findByIdAndDelete(userId);
+
+    res.send("Data is Updated succesfully!");
+  } catch (error) {
+    res.status(400).send("Something went wrong ");
   }
 });
 
