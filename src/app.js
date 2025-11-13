42718f1cd958a2c6d6ba480599d7a1d347e0e6c3
Rohit Singh <rajputrohit2k@gmail.com>
2025-11-13 11:28:23 +0530

    implemented token

--- src/app.js
+++ src/app.js
@@ -3,29 +3,80 @@ require("dotenv").config(); // Load .env
 const express = require("express");
 const connectDB = require("./config/database");
 const User = require("./models/user");
+const { validateSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
+const validator = require("validator");
+const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const { userAuth } = require("./middlewares/auth");
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 // Signup route
 app.post("/signup", async (req, res) => {
-  const user = new User(req.body);
-
   try {
+    // validation of data
+    validateSignUpData(req);
+
+    const { firstName, lastName, emailId, password } = req.body;
+    // Encrypt the password
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    // creating a new instance of the user model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId,
+      password: passwordHash,
+    });
+
     if (user?.skills.length > 10) {
       throw new Error("Skills cannot be more than 10");
     }
     await user.save();
     res.send("User Added successfully!");
   } catch (err) {
-    console.error(err);
-    res.status(400).send("Error saving the user" + err);
+    res.status(400).send("ERROR : " + err);
+  }
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { emailId, password } = req.body;
+
+    if (!validator.isEmail(emailId)) {
+      throw new Error("Invalid Cred");
+    }
+
+    const user = await User.findOne({ emailId: emailId });
+
+    if (!user) {
+      throw new Error("Invalid Cred");
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (isPasswordValid) {
+      // create a JWT Token
+      const token = await jwt.sign({ _id: user._id }, "DEV@Tinder@0870", {
+        expiresIn: "1d",
+      });
+
+      // Add the token to cookie and send the response back to the user
+      res.cookie("token", token, { expires: true });
+      res.send("Login Successful!!!");
+    } else {
+      throw new Error("Invalid Cred");
+    }
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
   }
 });
 
 app.get("/user", async (req, res) => {
-  console.log(req.body);
   const userEmail = req.body.emailId;
   try {
     const users = await User.find({ emailId: userEmail });
@@ -62,7 +113,6 @@ app.delete("/user", async (req, res) => {
 });
 
 app.patch("/user/:userId", async (req, res) => {
-  // console.log(req.body);
   const userID = req.params?.userId;
   const data = req.body;
 
@@ -93,6 +143,25 @@ app.patch("/user/:userId", async (req, res) => {
   }
 });
 
+app.get("/profile", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+
+    res.send(user);
+  } catch (error) {
+    res.status(400).send("Something went wrong " + error);
+  }
+});
+
+app.post("/sendConnectionRequest", userAuth, async (req, res) => {
+  try {
+    const user = req.user;
+    console.log("Sending a Connection request ");
+    res.send(user.firstName + " send the Connection Request Sent!");
+  } catch (error) {
+    res.status(400).send("ERROR  : " + error.message);
+  }
+});
 connectDB()
   .then(() => {
     console.log("Database connection done...");
--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,16 +1,31 @@
-const adminAuth = (req, res, next) => {
-  const token = "xyz";
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
 
-  const tokenAutohirze = token === "xyzn";
+const userAuth = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
 
-  if (tokenAutohirze) {
-    console.log("user is verified");
+    if (!token) {
+      throw new Error("Token is not valid ");
+    }
+
+    const decodedObj = await jwt.verify(token, "DEV@Tinder@0870");
+
+    const { _id } = decodedObj;
+
+    const user = await User.findById(_id);
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    req.user = user;
     next();
-  } else {
-    res.status(401).send("user is not authorize");
+  } catch (error) {
+    res.status(400).send("ERROR : " + error.message);
   }
 };
 
 module.exports = {
-  adminAuth,
+  userAuth,
 };
