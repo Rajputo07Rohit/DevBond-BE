1453c77b704d6e3e50983a4b5f6e71f2f470985f
Rohit Singh <rajputrohit2k@gmail.com>
2025-11-19 11:55:43 +0530

    feed api is done

--- apiList.md
+++ apiList.md
@@ -16,5 +16,7 @@ Status: ignore , interested , accepeted , rejected
 - POST/ request / review / accepted/ :requestId done ✅
 - POST/ request / review / rejected / :requestId done ✅
 
-- GET / connections not done ✅
-- GET / requests/received not done ✅
+- GET / connections done ✅
+- GET / requests/received done ✅
+
+/feed?page = 1&limit=10 => first 10 users 1-10;
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequestModel = require("../models/connectionRequest");
+const User = require("../models/user");
+
 const userRouter = express.Router();
 
 // Get all the pending connection request for the loggedIn user
@@ -27,7 +29,7 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
       data: connectionRequest,
     });
   } catch (err) {
-    res.status(404).send("ERROR : " + err.message);
+    res.status(400).send("ERROR : " + err.message);
   }
 });
 
@@ -54,6 +56,41 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
     });
 
     res.json({ data });
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+userRouter.get("/feed", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+
+    const skip = (page - 1) * limit;
+
+    const connectionRequest = await ConnectionRequestModel.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId  toUserId");
+
+    const hideUsersFromFeed = new Set();
+    connectionRequest.forEach((req) => {
+      hideUsersFromFeed.add(req.fromUserId.toString());
+      hideUsersFromFeed.add(req.toUserId.toString());
+    });
+
+    const users = await User.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUsersFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+      .select(USER_SAFE_DATA)
+      .skip(skip)
+      .limit(limit);
+
+    res.send(users);
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
   }
